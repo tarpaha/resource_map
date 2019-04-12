26404ce571f2a7665132564cac673f465d1360f4
tarpaha <ppopchikovsky@gmail.com>
2019-04-12 18:31:06 +0300

    show error message and finish with err code 1 if error occur

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::process;
+
 mod resource_map;
 
 fn main() {
 
     let xml = r#"
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" enc oding="utf-8"?>
 <ResourceMapData>
   <Bundles>
     <Bundle Filename="bundle01" DownloadSize="42">
@@ -13,6 +15,12 @@ fn main() {
 </ResourceMapData>
 "#;
 
-    let rm = resource_map::read_resource_map(xml);
-    println!("{:?}", rm);
+    let resource_map = resource_map::read_resource_map(xml);
+    match resource_map {
+        Ok(resource_map) => println!("{:?}", resource_map),
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1);
+        }
+    }
 }
